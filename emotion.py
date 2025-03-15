4b078067095a0d306888a0d675f1d7bcb5fb6383
Amaan <62588535+btw-its-amaan@users.noreply.github.com>
2025-03-15 12:02:30 +0530

    Add files via upload

---
+++ emotion.py
@@ -0,0 +1,13 @@
+emotion = "v.v"
+
+def main():
+    global emotio
+    say("Is there anybody out there?")
+    emotion = "o.o"
+    say("Oh, Hi!")
+    
+
+def say(phrase):
+    print(phrase + " " + emotion)
+
+main()
